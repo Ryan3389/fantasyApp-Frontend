3b9f0ff38635d75482837cf9fee666e7e37e9a50
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-04-24 17:32:55 -0400

    added vite config and fetch request

--- src/pages/HrPage.jsx
+++ src/pages/HrPage.jsx
@@ -1,4 +1,25 @@
+import { useState, useEffect } from "react"
 function HrPage() {
+    const [homeRunCount, setHomeRunCount] = useState(100)
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`https://fantasyapp-production-253f.up.railway.app/players/totalHomeRuns?homeRuns=${encodeURIComponent(homeRunCount)}`)
+                // const response = await fetch(`http://fantasyapp-production-253f.up.railway.app/players/totalHomeRuns?homeRuns=${encodeURIComponent(homeRunCount)}`)
+
+                if (!response.ok) {
+                    throw new Error
+                }
+
+                const data = await response.json()
+                console.log(data)
+            } catch (error) {
+                console.error(error)
+            }
+
+        }
+        fetchData()
+    }, [])
     return (
         <h1>Home Runs</h1>
     )
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+    proxy: {
+      '/players': {
+        target: 'https://fantasyapp-production-253f.up.railway.app',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
